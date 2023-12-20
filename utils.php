17d05019911e41d6ab12e358e920db43ce7f9c7f
Huang <hoanghung.ict@gmail.com>
2023-12-20 16:55:04 +0700

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,5 +1,24 @@
     <?php 
-    
+
+    public function copyFile() {
+           $dir = opendir($src);
+            @mkdir($dst);
+            while(false !== ( $file = readdir($dir)) ) {
+                if (( $file != '.' ) && ( $file != '..' )) {
+                    if ( is_dir($src . '/' . $file) ) {
+                        recurse_copy($src . '/' . $file,$dst . '/' . $file);
+                    }
+                    else {
+                        copy($src . '/' . $file,$dst . '/' . $file);
+                    }
+                }
+            }
+            closedir($dir);
+
+
+ }
+
+
     public function renameFile() {
         
         $directory = '/xampp/htdocs/Rac/';
